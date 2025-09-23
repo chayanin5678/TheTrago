348762d54df646629e352872bf4572e8681bb98b
chayanin5678 <chayanin4534@gmail.com>
2025-09-23 09:29:49 +0700

    revert: restore version 1.0.4 (2) for App Store submission

--- app.config.js
+++ app.config.js
@@ -14,7 +14,7 @@ module.exports = {
       deeplink: { host: "thetrago", path: "/", subdomain: "thetrago" },
       eas: { projectId: "74ac187e-236a-48db-8ae4-5aaa93e55f9a" }
     },
-  version: "1.0.5",
+  version: "1.0.4",
     android: {
       package: "com.chayanin5678.TheTrago"
     },
@@ -63,7 +63,7 @@ module.exports = {
 
     ios: {
       bundleIdentifier: "com.thetrago.ios",
-  buildNumber: "3",
+  buildNumber: "2",
       supportsTablet: true,
       requireFullScreen: false,
       userInterfaceStyle: "automatic",
